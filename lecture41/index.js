30bcf1cbc55fe345b79500e913f54f5ef7fc9c9a
algohercules <adarshrreso@gmail.com>
2025-01-21 12:01:53 +0530

    operators

---
+++ lecture41/index.js
@@ -0,0 +1,62 @@
+//airthmetic operators
+
+console.log("adarsh")
+
+
+
+let a = 10;
+let b = 5;
+console.log(a+b)
+
+
+
+// (a=a+10) = (a+=10)
+// (a=a-10) = (a-=10)
+// (a=a*3) = (a*=3)
+
+
+//pre increment
+//post increment
+//pre decrement
+//post decrement
+
+
+//comparison operators
+/*
+<
+>
+<=
+>=
+==
+!=
+!==
+*/
+
+console.log(10<5)
+console.log(10>5)
+console.log(10<=5)
+console.log(10>=5)
+console.log(10==5)
+console.log(10!=5)
+console.log(10!==5)
+
+console.log('5'==5);   //loose equality
+console.log('5'===5)   //strict equality
+
+
+//ternary operator
+
+// (condition) ? value1(for true) : value2(for false)
+
+let age = 25;
+let status1 = (age > 18)  ?  'i can vote' : 'i can not vote';
+console.log(status1)
+
+//logical operators
+/* 
+and (&&)
+or (||)
+not (!)
+*/
+
+
