4a78722e527f6d2aa59a636cb3fbf1e9c6b896c0
algohercules <adarshrreso@gmail.com>
2025-01-22 09:50:07 +0530

    creating an object

---
+++ lecture44/index.js
@@ -0,0 +1,11 @@
+// let obj = {
+//     "name":"adarsh",
+//     age : 25,
+//     weight : 85,
+//     height : "6 feet 1 inch",
+//     greet: function() {
+//         console.log("hello ji kaise ho saare")
+//     }
+// };
+// console.log(obj)
+// obj.greet();
