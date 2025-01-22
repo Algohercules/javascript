d977b9054055324a7de9967d2ea791aac7606c63
algohercules <adarshrreso@gmail.com>
2025-01-22 06:38:27 +0530

    .

--- lecture41/index.js
+++ lecture41/index.js
@@ -1,12 +1,12 @@
 //airthmetic operators
 
-console.log("adarsh")
+// console.log("adarsh")
 
 
 
-let a = 10;
-let b = 5;
-console.log(a+b)
+// let a = 10;
+// let b = 5;
+// console.log(a+b)
 
 
 
@@ -32,25 +32,25 @@ console.log(a+b)
 !==
 */
 
-console.log(10<5)
-console.log(10>5)
-console.log(10<=5)
-console.log(10>=5)
-console.log(10==5)
-console.log(10!=5)
-console.log(10!==5)
+// console.log(10<5)
+// console.log(10>5)
+// console.log(10<=5)
+// console.log(10>=5)
+// console.log(10==5)
+// console.log(10!=5)
+// console.log(10!==5)
 
-console.log('5'==5);   //loose equality
-console.log('5'===5)   //strict equality
+// console.log('5'==5);   //loose equality
+// console.log('5'===5)   //strict equality
 
 
 //ternary operator
 
 // (condition) ? value1(for true) : value2(for false)
 
-let age = 25;
-let status1 = (age > 18)  ?  'i can vote' : 'i can not vote';
-console.log(status1)
+// let age = 25;
+// let status1 = (age > 18)  ?  'i can vote' : 'i can not vote';
+// console.log(status1)
 
 //logical operators
 /* 
@@ -58,5 +58,126 @@ and (&&)
 or (||)
 not (!)
 */
+
+
+//working with non booleans 
+
+/*falsey
+undefined
+null
+0
+false
+NaN
+
+truthy--> anything that is not falsey*/
+
+
+//bitwise operators
+/*
+AND(&)
+OR(|)
+NOT(~)
+<<
+>>
+XOR(^)
+*/
+
+// console.log(2&5)
+// console.log(2|5)
+// console.log(~0)
+
+//left shift --> (5<<3) = (5*2^3)
+//right shift --> (10>>3) = (10/2^3)
+
+
+
+//conditionals:--
+
+/* 
+if(condition)
+{
+........
+}
+else
+{
+........
+}
+*/
+
+// let age1=8;
+// if(age1>18){
+//     console.log('can vote')
+// }
+// else{
+//     console.log('cannot vote')
+// }
+
+/*
+if(condition1)
+{
+.........
+}
+else-if(condition2)
+{
+.........
+}
+else-if(condition3)
+{
+...........
+}
+else
+{
+.........
+}
+*/
+
+//nested if-else
+
+// let number = 4;
+// let age2 = 104;
+
+// if(number == 1)
+// {
+//     console.log('a')
+// }
+// else if(number == 2)
+// {
+//     console.log('b')
+// }
+// else if(number == 3)
+//     {
+//         console.log('c')
+//     }
+// else if(number == 4)
+//         {
+//             if(age2>18){
+//                 console.log('i can vote')
+//             }   
+//             console.log('d')
+//         }
+//        else                                  //optional
+//             {
+//                 console.log('e')
+//             }
+
+
+//switch case
+
+// let num = 3;
+
+// switch(num){
+//     case 1: console.log('a');
+//     break;         
+//     case 2: console.log('b')
+//     break;
+//     case 3: console.log('c')
+//     break;
+//     case 4: console.log('d')
+//     break;
+//     default: console.log('g')
+// }
+
+
+
 
 
