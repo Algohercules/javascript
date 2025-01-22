00dddc2b57d4278d7b6b4fa8ce94696dcfb360b3
algohercules <adarshrreso@gmail.com>
2025-01-22 20:31:42 +0530

    reduce property

--- lecture44/index.js
+++ lecture44/index.js
@@ -48,17 +48,17 @@
 
 
 //filter:--
-let arr = [10,20,30,11,21,44,51]
- let evenArray = arr.filter( function (number){
-    if(number%2===0){
-        return true;
-    }
-    else{
-        return false;
-    }
-});
+// let arr = [10,20,30,11,21,44,51]
+//  let evenArray = arr.filter( function (number){
+//     if(number%2===0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// });
 
-console.log(evenArray)
+// console.log(evenArray)
 
 //or
 
