776c8cc90a89c6aefda904c28be91f2833083963
algohercules <adarshrreso@gmail.com>
2025-01-22 20:26:25 +0530

    filter property

--- lecture44/index.js
+++ lecture44/index.js
@@ -39,26 +39,26 @@
 
 //mapping:--
 
-let arr = [10,20,30];
+// let arr = [10,20,30];
 
- let ansArray = arr.map( function (number) {
-    return number*number;
-})
-console.log(ansArray)
+//  let ansArray = arr.map( function (number) {
+//     return number*number;
+// })
+// console.log(ansArray)
 
 
 //filter:--
-// let arr = [10,20,30,11,21,44,51]
-//  let evenArray = arr.filter((number)=>{
-//     if(number%2===0){
-//         return true;
-//     }
-//     else{
-//         return false;
-//     }
-// });
+let arr = [10,20,30,11,21,44,51]
+ let evenArray = arr.filter( function (number){
+    if(number%2===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+});
 
-// console.log(evenArray)
+console.log(evenArray)
 
 //or
 
