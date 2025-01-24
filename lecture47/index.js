7d3481cf0c5a2b317b4033ef0918ceab504fb836
algohercules <adarshrreso@gmail.com>
2025-01-25 00:42:38 +0530

    classes and kthe object

---
+++ lecture47/index.js
@@ -0,0 +1,5 @@
+// classes and default parameters
+
+// classes are the blueprints in which it is defined that which of the data members are going to be stoerd for a particualr entity
+
+// the actual data created of the classes are known as the  object
