32133d8ce6446b48de142c47eaf7eb1cab49f82b
algohercules <adarshrreso@gmail.com>
2025-01-25 09:17:17 +0530

    spread opearator

--- lecture49/index.js
+++ lecture49/index.js
@@ -11,3 +11,20 @@
 // obj.color = "white"
 
 // console.log(obj);
+
+//cloning : spred oprtr,
+
+//spread operator :--
+
+ let src = {
+         age:12,
+         wt:36,
+         ht:45
+  }
+
+let destination = {...src}
+
+src.age = 90;
+
+console.log("src:", src);
+console.log("destination:", destination)
