cd64fb7a980c8afd0d786290d8022a66e82c2303
algohercules <adarshrreso@gmail.com>
2025-01-25 09:19:53 +0530

    the difference between copying and cloning

--- lecture49/index.js
+++ lecture49/index.js
@@ -12,19 +12,47 @@
 
 // console.log(obj);
 
-//cloning : spred oprtr,
+
+
+
+
+//cloning : 
+
 
 //spread operator :--
 
- let src = {
-         age:12,
-         wt:36,
-         ht:45
-  }
+//  let src = {
+//          age:12,
+//          wt:36,
+//          ht:45
+//   }
 
-let destination = {...src}
+// let destination = {...src}
 
-src.age = 90;
+// src.age = 90;
 
-console.log("src:", src);
-console.log("destination:", destination)
+// console.log("src:", src);
+// console.log("destination:", destination)
+
+
+// the difference between copying and the cloning
+
+
+
+//spread operator :--
+
+//  let src = {
+//          age:12,
+//          wt:36,
+//          ht:45
+//   }
+
+// let destination = src;
+
+// src.age = 90;
+
+// console.log("src:", src);
+// console.log("destination:", destination)
+
+
+
