0e924df2bdf6c75f896c7f07c4bc4a4ef37112c8
algohercules <adarshrreso@gmail.com>
2025-01-26 18:08:17 +0530

    event target

--- lecture53/index.js
+++ lecture53/index.js
@@ -3,4 +3,5 @@
 //  event :---
 //  the process in which any announcement is made to the document i.e. mouseover, scroll, click etc is called an event
 
-event-target :---
+// event-target :---
+// the position at which event happens in a document is called as event target....
