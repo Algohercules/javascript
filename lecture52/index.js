704d4f1281506e827cc6c5a928c1b847aa341dab
algohercules <adarshrreso@gmail.com>
2025-01-26 17:58:01 +0530

    starting browser events

--- lecture52/index.js
+++ lecture52/index.js
@@ -1,3 +1,6 @@
 // css text property is uded to change multiple stylings in html using console window
 
-// .setattribute() is used to set any desired attribute to any element 
+// .setattribute() is used to set any desired attribute to any element using its class even if there is any styling using inline css 
+
+//this method overrides the previous value and gets precedence 
+
