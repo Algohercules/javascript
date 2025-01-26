997089559e5547bf212010ba47f5d872769bb7f2
algohercules <adarshrreso@gmail.com>
2025-01-26 18:13:12 +0530

    add event listener

--- lecture53/index.js
+++ lecture53/index.js
@@ -3,8 +3,12 @@
 //  event :---
 //  the process in which any announcement is made to the document i.e. mouseover, scroll, click etc is called an event
 
+
 // event-target :---
 // the position at which event happens in a document is called as event target....
 
+
 // event listener :---
 // the location at which the code is written to execute the happening event is called the event listener
+// this is done by the addEventListener()and removeEventListener
+
