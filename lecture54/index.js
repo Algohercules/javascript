1fa19a63e8a2b4cde8fd6a9755292bda349b0ea1
algohercules <adarshrreso@gmail.com>
2025-01-27 21:42:10 +0530

    calculating time taken by by code

--- lecture54/index.js
+++ lecture54/index.js
@@ -3,12 +3,26 @@
 
 //code 1
 
+const t1 = performance.now()
+
 for (let i = 1; i<100; i++){
     let para = document.createElement('p')
-    para.textContent = "this is para", +i;
+    para.textContent = "this is para" +i;
     document.body.appendChild(para);
 }
+const t2 = performance.now()
 
+console.log("total time taken by code 1:", + (t2 - t1));
 
 //code 2
 
+const t3 = performance.now()
+let myDiv = document.createElement('div')
+
+for( let i = 1; i<=100; i++){
+    let para = document.createElement('p')
+    para.textContent = "this ia para" +i;
+    myDiv.appendChild(para);
+}
+
+document.body.appendChild(myDiv)
