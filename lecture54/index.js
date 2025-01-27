5ed36b3b162407c8e22209e447ea86a7f18fef53
algohercules <adarshrreso@gmail.com>
2025-01-27 21:29:57 +0530

    code 1 : non efficient

--- lecture54/index.js
+++ lecture54/index.js
@@ -1,1 +1,14 @@
 // performance:---
+// the measure of efficienvy and measurement of the code is called as performance
+
+//code 1
+
+for (let i = 1; i<100; i++){
+    let para = document.createElement('p')
+    para.textContent = "this is para", +i;
+    document.body.appendChild(para);
+}
+
+
+//code 2
+
