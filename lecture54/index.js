ea9b1724d1b9a5ee2de19f2762fe2e0fe3a2c5db
algohercules <adarshrreso@gmail.com>
2025-01-27 23:30:23 +0530

    use of fragment

--- lecture54/index.js
+++ lecture54/index.js
@@ -3,33 +3,33 @@
 
 //code 1
 
-const t1 = performance.now()
+// const t1 = performance.now()
 
-for (let i = 1; i<100; i++){
-    let para = document.createElement('p')
-    para.textContent = "this is para" +i;
-    document.body.appendChild(para);
-}
-const t2 = performance.now()
+// for (let i = 1; i<100; i++){
+//     let para = document.createElement('p')
+//     para.textContent = "this is para" +i;
+//     document.body.appendChild(para);
+// }
+// const t2 = performance.now()
 
-console.log("total time taken by code 1:", + (t2 - t1));
+// console.log("total time taken by code 1:", + (t2 - t1));
 
-//code 2
+// //code 2
 
-const t3 = performance.now()
-let myDiv = document.createElement('div')
+// const t3 = performance.now()
+// let myDiv = document.createElement('div')
 
-for( let i = 1; i<=100; i++){
-    let para = document.createElement('p')
-    para.textContent = "this ia para" +i;
-    myDiv.appendChild(para);
-}
+// for( let i = 1; i<=100; i++){
+//     let para = document.createElement('p')
+//     para.textContent = "this ia para" +i;
+//     myDiv.appendChild(para);
+// }
 
-document.body.appendChild(myDiv);
+// document.body.appendChild(myDiv);
 
-const t4 = performance.now()
+// const t4 = performance.now()
 
-console.log("total time taken by code 2:", + (t3 - t4));
+// console.log("total time taken by code 2:", + (t3 - t4));
 
 
 
@@ -48,3 +48,21 @@ console.log("total time taken by code 2:", + (t3 - t4));
 
 
 //code 2 is faster bcz  there is very less reflow needed here due to the fact that we are first inserting the 100 paragraphhs in our div which requires no reflow and then we just simply painted that value on the web so it takes less reflow and repaint
+
+
+
+// document fragment :---
+//  it is a light weight document object where no reflow and repaint is done 
+
+
+//best code using document fragment:--
+
+let fragment = document.createDocumentFragment();
+
+for (let i = 1; i <= 100;  i++){
+    let para = document.createElement('p');
+    para.textContent = "this is para" +i;
+    fragment.appendChild(para);
+}
+
+document.body.appendChild(fragment);
