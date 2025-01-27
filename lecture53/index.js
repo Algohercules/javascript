81506832a16bddb53a25429d2001b3d8e92443cd
algohercules <adarshrreso@gmail.com>
2025-01-27 20:04:26 +0530

    too many listeners process

--- lecture53/index.js
+++ lecture53/index.js
@@ -61,3 +61,11 @@
 
 
 // avoiding too many listeners
+
+let paras = document.querySelectorAll('p');
+for(let i = 0; i<paras.length; i++){
+    let para = paras[i];
+    para.addEventListener('click', function(){
+        alert("you have clicked on para:" + i)
+    })
+}
