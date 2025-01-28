c84529b9b29cb66b7656d7b36bd0f664a5e40380
algohercules <adarshrreso@gmail.com>
2025-01-28 07:37:50 +0530

    synchronous code resolve method

--- lecture56/index.js
+++ lecture56/index.js
@@ -1,3 +1,12 @@
 // promise :---
 // the promise object represents the eventual completion or fsilure of an asynchronous code and its resulting value
 
+//  resolve/fulfill State
+//  reject state
+
+// let firstPromise = new Promise ((resolve , reject) => {
+//     console.log("babbar")
+//     resolve(1001)
+
+// })
+
