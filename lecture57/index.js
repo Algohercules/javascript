6b13a61f0d1442079da72fe6548224067dfc877f
algohercules <adarshrreso@gmail.com>
2025-01-28 19:06:16 +0530

    await and fetch api

---
+++ lecture57/index.js
@@ -0,0 +1,20 @@
+// <!-- //async-await :---
+// this property is used to provide synchronous behaviour to asynchronous code -->
+
+//this is an asynchronous code
+async function getData(){
+    setTimeout(function(){
+        console.log("i am inside timeout block", 3000);
+    })
+}
+
+let output = getData();
+
+// await ???????
+
+// fetch API :---
+
+
+
+
+
