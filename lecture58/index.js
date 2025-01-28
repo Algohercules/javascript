e56365724374c8cbcb745b051cb17af8fbf1766b
algohercules <adarshrreso@gmail.com>
2025-01-28 23:12:17 +0530

    emclosures left

--- lecture58/index.js
+++ lecture58/index.js
@@ -1,10 +1,10 @@
 //  closure = {function + (required data) or (lexical scope) or (surrounding state)}
 
-function init(){
-    let name = "mozilla":
-    function displayName(){
-        console.log(name);
-    }
-    displayName();
-}
-init();
+// function init(){
+//     let name = "mozilla";
+//     function displayName(){
+//         console.log(name);
+//     }
+//     displayName();
+// }
+// init();
